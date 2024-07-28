0a3c7366189444639bf8bf8ad99fc7809cc5f8c5
Morten rasmussen <97233551+Morten010@users.noreply.github.com>
2024-07-28 18:23:15 +0200

    ad

---
+++ components/homepage/CategoryShowcase.vue
@@ -0,0 +1,96 @@
+<script lang="ts" setup>
+    const {
+        fetchUrl,
+        moreUrl,
+        title
+    } = defineProps<{
+        title: string
+        moreUrl: string
+        fetchUrl: string
+    }>()
+</script>
+
+<template>
+    <section
+        class="w-full mb-4"
+    >
+        <!-- top -->
+        <div
+        class="flex justify-between"
+        >
+            <h2
+                class="text-2xl text-green-950 font-bold"
+            >
+                {{ title }}
+            </h2> 
+            <button
+                class="font-bold text-green-950 flex items-center gap-2 hover:bg-green-500/10 px-2 rounded transition-colors duration-100"
+            >
+                Se alle
+                <Icon 
+                    name="lucide:arrow-right"
+                />
+            </button>
+        </div>
+        <!-- top -->
+
+        <div
+            class="mt-3 flex gap-2"
+        >
+            <div>
+                <div>
+                    <NuxtImg 
+                        src="https://placehold.co/250x250"
+                        class="rounded-xl"
+                    />
+                </div>
+                <h3
+                    class="mt-2"
+                >
+                    Pariser Toast
+                </h3>
+                <p
+                    class="text-sm text-black/60 font-medium"
+                >
+                    best toast i byen
+                </p>
+            </div>
+            <div>
+                <div>
+                    <NuxtImg 
+                        src="https://placehold.co/250x250"
+                        class="rounded-xl"
+                    />
+                </div>
+                <h3
+                    class="mt-2"
+                >
+                    Pariser Toast
+                </h3>
+                <p
+                    class="text-sm text-black/60 font-medium"
+                >
+                    best toast i byen
+                </p>
+            </div>
+            <div>
+                <div>
+                    <NuxtImg 
+                        src="https://placehold.co/250x250"
+                        class="rounded-xl"
+                    />
+                </div>
+                <h3
+                    class="mt-2"
+                >
+                    Pariser Toast
+                </h3>
+                <p
+                    class="text-sm text-black/60 font-medium"
+                >
+                    best toast i byen
+                </p>
+            </div>
+        </div>
+    </section>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,25 @@
-<script setup lang="ts">
-    const { data, error, refresh } = await useFetch('/api/recipe')
-</script>
-
 <template>
     <h1
-        class="text-4xl font-bold text-green-950"
+        class="text-6xl font-bold text-green-950 leading-[55px] mb-4"
     >
         Klar til at lave mad?
     </h1>
-    <p
-    class="mb-2"
-    >
-        Velkommen tilbagge
-    </p>
     
-    <div>
-       <ul>
-            <li
-                v-for="recipe of data"
-            >
-                {{ recipe.name }}
-            </li>
-       </ul>
-    </div>
+    <HomepageCategoryShowcase 
+        fetch-url="/api/category/1"
+        more-url="/category/1"
+        title="Morgenmad"
+    />
+    <HomepageCategoryShowcase 
+        fetch-url="/api/category/1"
+        more-url="/category/1"
+        title="Frokost"
+    />
+    <HomepageCategoryShowcase 
+        fetch-url="/api/category/1"
+        more-url="/category/1"
+        title="Aftensmad"
+    />
+
+    
 </template>
